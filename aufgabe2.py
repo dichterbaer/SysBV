b664e6ee6395a70de11fe72bbd9b66f4517f0cf0
Maximilian Stempel <maximilian.stempel@googlemail.com>
2022-05-26 13:17:30 +0200

    a2 angefangen

--- aufgabe2.py
+++ aufgabe2.py
@@ -1,7 +1,16 @@
 import numpy as np
 import cv2
 
+def X_cos(t): #Cosinuswerte im Intervall
+    Xout = np.cos(t)
+    return  Xout
 
+
+def K_fft(signal):
+    w = np.arange(0,signal.shape[0],1)
+    T, S = np.meshgrid(w, w)       
+    k_out = np.exp(-2*np.pi*1j*S*T/signal.shape[0]) 
+    return  k_out
 #
 # Aufgabe 1b)
 k = np.array([1,4,6,4,1])*(1/16)
@@ -41,5 +50,14 @@ cv2.imshow('Gefiltert OpenCV', lena_glatt_cv)
 cv2.imshow('Differenz Händisch vs OpenCV', diff)
 cv2.waitKey(0)
 
+
+#Aufgabe 2a)
+n = 2**10
+x = np.linspace(0, 200*np.pi, n)
+
+cos = X_cos(x)
+f_h = np.matmul(K_fft(x), cos)
+
 print()
+
 
