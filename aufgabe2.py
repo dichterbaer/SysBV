05cb891f11fdb9f774200cc7b25cbc9b085887dc
Maximilian Stempel <maximilian.stempel@googlemail.com>
2022-05-27 18:43:13 +0200

    a1 gefixt

--- aufgabe2.py
+++ aufgabe2.py
@@ -19,42 +19,42 @@ def K_fft(signal):
     return  k_out
 #
 # Aufgabe 1b)
-# k = np.array([1,4,6,4,1])*(1/16)
-# x = np.arange(0, 21, 1)
-# kk= np.transpose(np.zeros(x.size))
+k = np.array([1,4,6,4,1])*(1/16)
+x = np.arange(0, 21, 1)
+kk= np.transpose(np.zeros(x.size))
 
-# kk[0:k.size]=k
-# K = kk
+kk[0:k.size]=k
+K = kk
 
-# #K[0:kk.size]= kk[0:kk.size]
-# for i in range(0, x.size-1): #create K with shifted k
-#     kk = np.roll(kk, 1)
-#     #K = np.append(K , k_temp)
-#     K = np.vstack((K, kk))
+#K[0:kk.size]= kk[0:kk.size]
+for i in range(0, x.size-1): #create K with shifted k
+    kk = np.roll(kk, 1)
+    #K = np.append(K , k_temp)
+    K = np.vstack((K, kk))
 
-# #apply K to x
-# yK = np.dot(K, np.transpose(x))
+#apply K to x
+yK = np.dot(K, np.transpose(x))
 
-# #vergleich np implementierung mit händisch (passt aber nicht)
-# yk_np = np.convolve(x, k)
+#vergleich np implementierung mit händisch (passt aber nicht)
+yk_np = np.convolve(x, k)
 
-# yk = np.zeros(x.size)
-# # letzter wert passt noch nicht ganz
-# #apply k to x 
-# for i in range(0, x.size-1): 
-#     yk[i] = np.sum(np.dot(x[0:5], k)) 
-#     x = np.roll(x, -1)
+yk = np.zeros(x.size)
+# letzter wert passt noch nicht ganz
+#apply k to x 
+for i in range(0, x.size-1): 
+    yk[i] = np.sum(np.dot(x[0:5], k)) 
+    x = np.roll(x, -1)
 
-# lena = cv2.imread('data/lena.png', 0)
-# cv2.imshow('Original', lena)
+lena = cv2.imread('data/lena.png', 0)
+cv2.imshow('Original', lena)
 
-# lena_glatt = cv2.filter2D(lena, -1, k)
-# lena_glatt_cv = cv2.GaussianBlur(lena, (1,5), 0)
-# diff = lena_glatt - lena_glatt_cv
-# cv2.imshow('Gefiltert Händisch', lena_glatt)
-# cv2.imshow('Gefiltert OpenCV', lena_glatt_cv)
-# cv2.imshow('Differenz Händisch vs OpenCV', diff)
-# cv2.waitKey(0)
+lena_glatt = cv2.filter2D(lena, -1, k)
+lena_glatt_cv = cv2.GaussianBlur(lena, (1,5), 0)
+diff = lena_glatt_cv - lena_glatt 
+cv2.imshow('Gefiltert Händisch', lena_glatt)
+cv2.imshow('Gefiltert OpenCV', lena_glatt_cv)
+cv2.imshow('Differenz Händisch vs OpenCV', diff)
+cv2.waitKey(0)
 
 #Aufgabe 2a) animiert (ist noch nicht richtig. sollte keine matrixmultiplikation sein sondern ein diskretisierter kernel)
 
