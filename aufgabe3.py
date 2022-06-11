df73fe815b2dc2eecf14e4a1a3b234e7826c1161
Maximilian Stempel <maximilian.stempel@googlemail.com>
2022-06-11 18:40:10 +0200

    a 3 + data

--- aufgabe3.py
+++ aufgabe3.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 
 def create_cos(N, alpha, beta, gamma):
-    x = np.linspace(0, 1, N)
+    x = np.linspace(0, 1, N+1)
+    x = x[:-2]
     X, Y = np.meshgrid(x, x)
     return np.cos(2*np.pi*(X*alpha + Y*beta + gamma))
 
@@ -37,5 +38,17 @@ cos_d = create_cos(256, a, b, c)
 fft_d = np.imag(np.fft.fft2(cos_d))
 cv2.imshow('cos_d', cos_d)
 cv2.imshow('fft_d', fft_d)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+#Aufgabe 2a)
+
+img = cv2.imread('data/how60c.tif', 0)
+img_fft = np.fft.fft2(img)
+img_fft_shift = np.fft.fftshift(img_fft)
+cv2.imshow('Original', img)
+cv2.imshow('Fourier', np.abs(img_fft))
+cv2.imshow('Fourier shift', np.abs(img_fft_shift))
 cv2.waitKey()
 cv2.destroyAllWindows()
